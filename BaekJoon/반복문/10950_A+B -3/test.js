95dc298671dda935c9af1aae65f2b130640a68c5
kkssbbb <03337@naver.com>
2022-09-11 17:26:08 +0900

    test c

--- BaekJoon/반복문/10950_A+B -3/test.js
+++ BaekJoon/반복문/10950_A+B -3/test.js
@@ -1,26 +1,6 @@
 const fs = require('fs');
 let input = fs.readFileSync('BaekJoon/반복문/10950_A+B -3/input.txt').toString().split("\n");
 
-let testCaseNum = [];
-testCaseNum = input.shift();
-
-console.log(testCaseNum);
 console.log(input);
 
 
-
-
-for(let i = 0; i<testCaseNum; i++){
-    let arr = input[i].split('')
-}
-console.log(arr);
-
-/*  */
-
-solution(input);
-
-function solution(n){
-
-
-
-}
