c6f6e926b2416c6d2ab5be8c372a30157f24a54d
kkssbbb <03337@naver.com>
2022-09-11 13:50:11 +0900

    어제커밋 안된거..

---
+++ BaekJoon/반복문/8393_합/test.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+let input = +fs.readFileSync('BaekJoon/반복문/8393_합/input.txt').toString().split("\n");
+
+console.log(input);
+
+for(let i=1; i<= input; i++){
+
+    let answer =0
+    
+
+}
