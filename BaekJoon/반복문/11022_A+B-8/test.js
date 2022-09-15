74c7700152a5935d228f2bbd6e343f6670ed120e
kkssbbb <03337@naver.com>
2022-09-15 15:09:42 +0900

    백준 반복문 커밋

--- BaekJoon/반복문/11022_A+B-8/test.js
+++ BaekJoon/반복문/11022_A+B-8/test.js
@@ -1,17 +1,2 @@
 const fs = require('fs');
-let input = fs.readFileSync('BaekJoon/반복문/11022_A+B-8/input.txt').toString().split('\n');
-
-let testNum = input[0];
-let answer = 0;
-/* console.log(input); */
-
-
-for(let i=1; i<=testNum; i++){
-    
-    let [a, b] = input[i].split(" ");
-    answer = (+a)+(+b)
-    console.log(`Case #${i}: ${a} + ${b} = ${answer}`);
-    
-
-}
-
+let input = fs.readFileSync('')
---
+++ BaekJoon/반복문/2438_별 찍기-1/test.js
@@ -0,0 +1,5 @@
+const fs = require('fs');
+let input = +fs.readFileSync('BaekJoon/반복문/2438_별 찍기-1/input.txt');
+
+console.log(input);
+
