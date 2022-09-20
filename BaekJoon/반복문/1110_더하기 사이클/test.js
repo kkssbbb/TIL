9bbf4006b2d1cc409c6ca94713319048f15413d0
kkssbbb <03337@naver.com>
2022-09-21 00:38:55 +0900

    백준 반복문 문제 올 클리어

--- BaekJoon/반복문/1110_더하기 사이클/test.js
+++ BaekJoon/반복문/1110_더하기 사이클/test.js
@@ -1,16 +1,22 @@
 const fs = require("fs");
-let input = fs.readFileSync('BaekJoon/반복문/10951_A+B-4/input.txt').toString().split('\n');
+let input = fs
+    .readFileSync('BaekJoon/반복문/1110_더하기 사이클/input.txt')
+    .toString();
 
+let n = +input;
+let sum;
+let count = 0;
 
-answer = 0;
-for(let i= 0; i<input.length-1; i++){
+while (true) {
+    count++;
+    // 10의자리 + 1의자리
+    sum = Math.floor(n / 10) + (n % 10);;
+    //1의자리 + sum
+    n = (n % 10) * 10 + sum % 10;
 
-    let [a, b] = input[i].split(" ");
-
-    answer = (+a)+(+b);
-
-    console.log(answer);
-
+    if (n == input) {
+        break;
+    }
 }
 
-
+console.log(count);
