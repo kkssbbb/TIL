1cb3d1ffbe6fd41c3026b9930014c66cb5e5b884
kkssbbb <03337@naver.com>
2022-09-30 19:59:36 +0900

    test

--- ETC/연습장/test.js
+++ ETC/연습장/test.js
@@ -1,7 +1,15 @@
-let mom = {
-  money :10000
-}
+// 배열 누적합 구하기
 
-let me = {
-  phone : 'iphone'
-}
+let arr = [1,2,3,4,5]
+
+
+/* let result =0;
+arr.forEach(num=>{
+  result += num;
+}); */
+
+let result = arr.reduce((arr,crr)=>{
+  return arr+crr;
+},0);
+
+console.log(result);
