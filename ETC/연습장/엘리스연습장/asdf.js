cf26772ad81890a69c7a88b22221ff8f46aa1915
kkssbbb <03337@naver.com>
2022-10-06 22:27:38 +0900

    연습장

--- ETC/연습장/엘리스연습장/asdf.js
+++ ETC/연습장/엘리스연습장/asdf.js
@@ -1,12 +1,18 @@
-let mom = {
-  money :10000
-}
-
-let me = {
-  phone : 'iphone'
-}
+const fetchNum = new Promise((resolve,reject)=>{
+  setTimeout(()=>{
+    resolve(1);
+  },1000)
+})
 
 
-me.prototype = mom ;
-
-console.log(me.hasOwnProperty("money"));
+fetchNum
+  .then((num) =>num*1)
+  .then(num => num+2)
+  .then(num => new Promise((resolve,resject)=>
+    setTimeout(()=>{
+        resolve(num*2)
+  },1000)
+  )
+  )
+  .then(num => console.log(num)
+  )
