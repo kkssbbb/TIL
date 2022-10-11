0db4bc69d094881d20c113758cf24cbfb50fd2a4
kkssbbb <03337@naver.com>
2022-10-11 10:21:06 +0900

    백준 손익분기점 풀이및 블로그

--- BaekJoon/기본수학 1/손익분기점/test.js
+++ BaekJoon/기본수학 1/손익분기점/test.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 let input = fs.readFileSync('BaekJoon/기본수학 1/손익분기점/input.txt').toString().split(' ')
 
 
-
+/* 
 let 가변비용 = +input[1];
 let 책정금액 = +input[2];
 let sum = 0;
@@ -24,3 +24,19 @@ while(sum != 책정금액sum){
     console.log(answer>2100000001? -1 : answer);
 }
 
+ */
+const 고정비용 = +input[0];
+let 가변비용 = +input[1];
+let 책정금액 = +input[2];
+
+// 고정비용/(책정금액-가정비용) +1
+// 가변비용>= 책정금액  손익분기점 없음 
+
+if(가변비용>=책정금액){
+    console.log(-1);
+    
+}else{
+    const answer = Math.floor(고정비용/(책정금액-가변비용)) +1;
+    console.log(answer);
+    
+}
