947b2347bfbfb868f70757a5d691b9689fe3642b
kkssbbb <03337@naver.com>
2022-10-12 22:01:23 +0900

    test

--- ETC/연습장/엘리스연습장/index.js
+++ ETC/연습장/엘리스연습장/index.js
@@ -1,7 +1,24 @@
-function adder_promise(a, b) {
-    return new Promise((resolve, reject) => {
-        resolve(a+b);
-    });
+function test(mm, yyyy) {
+    if (mm === 2 && 윤년판단(yyyy)) {
+        // answer(yyyy, mm, dd, 29);
+        console.log("윤년");
+
+    }
+    if (mm === 2 && !윤년판단(yyyy)) {
+        // answer(yyyy, mm, dd, 28);
+        console.log("윤년아님");
+
+    }
+
+    function 윤년판단(yyyy) {
+console.log(yyyy);
+
+        return ((yyyy % 4 == 0) && (yyyy % 100 != 0)) || (yyyy % 400 == 0);
+    }
 }
 
-module.exports = adder_promise;
+function 윤년판단2(yyyy) {     if (yyyy % 4 === 0 && yyyy % 100 != 0 || yyyy %
+400 === 0) {         return true;     } else {         return false;     } }
+test(2,2021)
+test(2, 2020)
+//console.log(윤년판단(2020));
