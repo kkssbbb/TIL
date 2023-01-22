0f1b8ed9f374a74aaffb7ff888d2d3f256f6e20f
kkssbbb <03337@naver.com>
2023-01-22 17:43:53 +0900

    feat: 동적프로그래밍 정리 파일 생성

--- Algorithm/tempCodeRunnerFile.js
+++ Algorithm/tempCodeRunnerFile.js
@@ -1,1 +1,29 @@
- [empty, 1991, empty, 5678, 1234]
+class Node {
+  constructor(){
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class Stack{
+  constructor(){
+    this.top= null;
+    this.bottom =null;
+    this.length = 0;
+  }
+
+  peek(){
+    return this.top;
+  }
+
+  push(v){
+    const newNode = new Node(v);
+    newNode.next = this.top;
+    this.top = newNode;
+  }
+  pop(){
+    if(this.length === 0) return;
+
+    this.top = this.top.next;
+  }
+}
